a5526044d5a2f7223a369af8268621f1c3b301a9
m-srikanth <srime97@gmail.com>
2024-01-16 11:24:59 +0000

    gggg

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,35 +1,129 @@
+import Popup from 'reactjs-popup'
+import {Component} from 'react'
 import CartContext from '../../context/CartContext'
 
 import './index.css'
 
-const CartSummary = () => (
-  <CartContext.Consumer>
-    {value => {
-      const {cartList} = value
-      let total = 0
-      cartList.forEach(eachCartItem => {
-        total += eachCartItem.price * eachCartItem.quantity
-      })
+class CartSummary extends Component {
+  state = {isDisabled: false, isSuc: false}
 
-      return (
-        <>
-          <div className="cart-summary-container">
-            <h1 className="order-total-value">
-              <span className="order-total-label">Order Total:</span> Rs {total}
-              /-
-            </h1>
-            <p className="total-items">{cartList.length} Items in cart</p>
-            <button type="button" className="checkout-button d-sm-none">
-              Checkout
-            </button>
-          </div>
-          <button type="button" className="checkout-button d-lg-none">
-            Checkout
-          </button>
-        </>
-      )
-    }}
-  </CartContext.Consumer>
-)
+  changeStat = () => {
+    this.setState(pre => ({isDisabled: !pre.isDisabled}))
+  }
+
+  sucPrint = () => {
+    this.setState(pre => ({isSuc: !pre.isSuc}))
+  }
+
+  render() {
+    const {isDisabled, isSuc} = this.state
+    console.log(isDisabled)
+
+    return (
+      <CartContext.Consumer>
+        {value => {
+          const {cartList} = value
+          let total = 0
+          let itemsNo = 0
+          cartList.forEach(eachCartItem => {
+            total += eachCartItem.price * eachCartItem.quantity
+            itemsNo += eachCartItem.quantity
+          })
+
+          return (
+            <>
+              <div className="cart-summary-container">
+                <h1 className="order-total-value">
+                  <span className="order-total-label">Order Total:</span> Rs{' '}
+                  {total}
+                  /-
+                </h1>
+                <p className="total-items">{cartList.length} Items in cart</p>
+                <Popup
+                  trigger={
+                    <button type="button" className="checkout-button d-sm-none">
+                      Checkout
+                    </button>
+                  }
+                  position="center"
+                >
+                  {close => (
+                    <div className="div1">
+                      <button type="button" onClick={() => close()}>
+                        close
+                      </button>
+                      <input type="checkbox" id="check" disabled="true" />
+                      <label htmlFor="check">Net Banking</label>
+                      <input
+                        type="checkbox"
+                        id="check"
+                        onClick={this.changeStat}
+                      />
+                      <label htmlFor="check">Cash on Delivery</label>
+                      {isDisabled && (
+                        <button
+                          type="button"
+                          className="checkout-button"
+                          onClick={this.sucPrint}
+                        >
+                          Confirm Order
+                        </button>
+                      )}
+                      {isSuc && <p>Your order has been placed successfully</p>}
+                      <h1 className="order-total-value">
+                        <span className="order-total-label">Total Cost:</span>{' '}
+                        Rs {total}
+                        /-
+                      </h1>
+                      <p>Number of Items: {itemsNo}</p>
+                    </div>
+                  )}
+                </Popup>
+              </div>
+              <Popup
+                trigger={
+                  <button type="button" className="checkout-button d-lg-none">
+                    Checkout
+                  </button>
+                }
+                position="right center"
+              >
+                {close => (
+                  <div className="div1">
+                    <button type="button" onClick={() => close()}>
+                      close
+                    </button>
+                    <input
+                      type="checkbox"
+                      id="check"
+                      onClick={this.changeStat}
+                    />
+                    <label htmlFor="check">Cash on Delivery</label>
+                    {isDisabled && (
+                      <button
+                        type="button"
+                        className="checkout-button"
+                        onClick={this.sucPrint}
+                      >
+                        Confirm Order
+                      </button>
+                    )}
+                    {isSuc && <p>Your order has been placed successfully</p>}
+                    <h1 className="order-total-value">
+                      <span className="order-total-label">Total Cost:</span> Rs{' '}
+                      {total}
+                      /-
+                    </h1>
+                    <p>Number of Items: {itemsNo}</p>
+                  </div>
+                )}
+              </Popup>
+            </>
+          )
+        }}
+      </CartContext.Consumer>
+    )
+  }
+}
 
 export default CartSummary
